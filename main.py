cc39025cf7ef69ea52bc6f9de330a207e538c11d
Betojr04 <betomasia12@gmail.com>
2024-11-24 18:44:31 -0700

    added user and portfolio models with relationship established between both'

--- main.py
+++ main.py
@@ -12,6 +12,32 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    hashed_password = db.Column(
+        db.String(1000),
+        nullable=False,
+    )
+
+    portfolios = db.relationship(
+        "Portfolio", back_populates="user", cascade="all, delete-orphan"
+    )
+
+
+class Portfolio(db.Model):
+    __tablename__ = "portfolios"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    user = db.relatinship("User", back_populates="portfolios")
+
+
 @app.route("/")
 def welcome():
     print("Welcome to Portfolio Tracker")
